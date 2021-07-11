33494862d56dff3e9567c0292ab5c771aef6f880
raavann <imraavann@gmail.com>
2021-07-11 22:02:49 +0530

    make selenium headless

--- contests/codechef.py
+++ contests/codechef.py
@@ -13,6 +13,8 @@ def create_browser():
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
 
     global codechef_browser 
     codechef_browser = webdriver.Chrome(options=chrome_options)
