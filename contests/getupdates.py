20470fe4e11dffc96ea8dc4cfd32c76903bb4eb3
raavann <imraavann@gmail.com>
2021-07-14 21:06:34 +0530

    edited time

--- contests/getupdates.py
+++ contests/getupdates.py
@@ -40,19 +40,20 @@ async def get_updates(client):
                     contest_data.update_rd1(int(dtime.id_))
                     dtime.day1_rem = True
                     em = embds.embed_1drem(contest_data.get_cont_by_id(int(dtime.id_)),client.user.avatar_url)
-                    print('1d rem sending..')
+                    print('1d rem sending..',dtime)
                     await send_updates(em,client)
                 elif( (dtime.time_ -timedelta(minutes=50)) < datetime.now() < (dtime.time_ -timedelta(minutes=20))  and dtime.hour1_rem == False):
                     contest_data.update_rh1(int(dtime.id_))
                     dtime.hour1_rem = True
                     em = embds.embed_1hrem(contest_data.get_cont_by_id(int(dtime.id_)),client.user.avatar_url)
-                    print('1h rem sending..')
+                    print('1h rem sending..',dtime)
                     await send_updates(em,client)
                 elif(datetime.now() > dtime.time_ and dtime.char_ == 'e'):
                     em = embds.embed_contest_ended(contest_data.get_cont_by_id(int(dtime.id_)))
                     await send_updates(em,client)
                     dtime.char_ = 'x' #garbage value so this dtime do not get to come here again
-                    print('contest ended..')
+                    print('contest ended..',dtime)
                     contest_data.remove_cont(int(dtime.id_))
-            print('outside for sleep 1h .. ', dtime)
-            await asyncio.sleep(300)
+            print('outside for sleep 15m .. ')
+            await asyncio.sleep(900)
+            print(datetime.now())
